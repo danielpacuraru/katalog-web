857d8397940bb5bb43f5b11aa54ab7cecd5f086e
Daniel Pacuraru <pacurarudaniel@gmail.com>
2022-01-19 12:47:25 +0200

    header

--- src/app/_components/main/main.component.ts
+++ src/app/_components/main/main.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '../../_models/auth';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.less']
 })
-export class MainComponent { }
+export class MainComponent {
+
+  public user: User;
+  public userShort: string;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+    this.user = this.route.snapshot.data.user;
+    this.userShort = this.user.name.split(' ').map(w => w[0]).join('').substring(0, 2);
+  }
+
+}
